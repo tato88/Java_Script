ce2b77d5fd3e31f72ed0a8b78632ca4bf8c9efea
tato88 <380685013386@ex.ua>
2022-03-11 03:06:52 +0200

    Lesson10 adds recursion done!

--- Lesson10/additional/main.js
+++ Lesson10/additional/main.js
@@ -32,112 +32,112 @@
 // pageReader()
 
 // -- взять массив пользователей
-let usersWithAddress = [
-    {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-    {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-    {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-    {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-    {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-    {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-    {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-    {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-    {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-    {id: 11, name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-];
+// let usersWithAddress = [
+//     {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+//     {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+//     {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+//     {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+//     {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+//     {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+//     {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+//     {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+//     {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+//     {id: 11, name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+// ];
 // - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
 
-let newArray = usersWithAddress
-let userBox = document.getElementById('usersBox')
-let inpFalse = document.getElementById('inp1')
-let inp29 = document.getElementById('inp2')
-let inpKyiv = document.getElementById('inp3')
-
-for (let user of newArray) {
-    let userDiv = document.createElement('div')
-    userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-    userBox.append(userDiv)
-}
-let functionForm = () => {
-    let newArray = usersWithAddress
-    if (inpFalse.checked) {
-        oldDiv = userBox.querySelectorAll('div');
-        for (let i = 0, len = oldDiv.length; i < len; i++) {
-            oldDiv[i].parentNode.removeChild(oldDiv[i]);
-        }
-        newArray = newArray.filter(value => !value.status)
-        for (let user of newArray) {
-            let userDiv = document.createElement('div')
-            userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-            userBox.append(userDiv)
-        }
-        if (!inpFalse.checked) {
-            oldDiv = userBox.querySelectorAll('div');
-            for (let i = 0, len = oldDiv.length; i < len; i++) {
-                oldDiv[i].parentNode.removeChild(oldDiv[i]);
-            }
-
-            for (let user of newArray) {
-                let userDiv = document.createElement('div')
-                userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-                userBox.append(userDiv)
-            }
-        }
-    }
-    if (inp29.checked) {
-        oldDiv = userBox.querySelectorAll('div');
-        for (let i = 0, len = oldDiv.length; i < len; i++) {
-            oldDiv[i].parentNode.removeChild(oldDiv[i]);
-        }
-        newArray = newArray.filter(value => value.age > 28)
-        for (let user of newArray) {
-            let userDiv = document.createElement('div')
-            userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-            userBox.append(userDiv)
-        }
-    }
-    if (!inp29.checked) {
-        oldDiv = userBox.querySelectorAll('div');
-        for (let i = 0, len = oldDiv.length; i < len; i++) {
-            oldDiv[i].parentNode.removeChild(oldDiv[i]);
-        }
-
-        for (let user of newArray) {
-            let userDiv = document.createElement('div')
-            userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-            userBox.append(userDiv)
-        }
-    }
-
-    if (inpKyiv.checked) {
-        oldDiv = userBox.querySelectorAll('div');
-        for (let i = 0, len = oldDiv.length; i < len; i++) {
-            oldDiv[i].parentNode.removeChild(oldDiv[i]);
-        }
-        newArray = newArray.filter(value => value.address.city === 'Kyiv')
-        for (let user of newArray) {
-            let userDiv = document.createElement('div')
-            userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-            userBox.append(userDiv)
-        }
-    }
-    if (!inpKyiv.checked) {
-        oldDiv = userBox.querySelectorAll('div');
-        for (let i = 0, len = oldDiv.length; i < len; i++) {
-            oldDiv[i].parentNode.removeChild(oldDiv[i]);
-        }
-
-        for (let user of newArray) {
-            let userDiv = document.createElement('div')
-            userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
-            userBox.append(userDiv)
-        }
-    }
-}
+// let newArray = usersWithAddress
+// let userBox = document.getElementById('usersBox')
+// let inpFalse = document.getElementById('inp1')
+// let inp29 = document.getElementById('inp2')
+// let inpKyiv = document.getElementById('inp3')
+//
+// for (let user of newArray) {
+//     let userDiv = document.createElement('div')
+//     userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//     userBox.append(userDiv)
+// }
+// let functionForm = () => {
+//     let newArray = usersWithAddress
+//     if (inpFalse.checked) {
+//         oldDiv = userBox.querySelectorAll('div');
+//         for (let i = 0, len = oldDiv.length; i < len; i++) {
+//             oldDiv[i].parentNode.removeChild(oldDiv[i]);
+//         }
+//         newArray = newArray.filter(value => !value.status)
+//         for (let user of newArray) {
+//             let userDiv = document.createElement('div')
+//             userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//             userBox.append(userDiv)
+//         }
+//         if (!inpFalse.checked) {
+//             oldDiv = userBox.querySelectorAll('div');
+//             for (let i = 0, len = oldDiv.length; i < len; i++) {
+//                 oldDiv[i].parentNode.removeChild(oldDiv[i]);
+//             }
+//
+//             for (let user of newArray) {
+//                 let userDiv = document.createElement('div')
+//                 userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//                 userBox.append(userDiv)
+//             }
+//         }
+//     }
+//     if (inp29.checked) {
+//         oldDiv = userBox.querySelectorAll('div');
+//         for (let i = 0, len = oldDiv.length; i < len; i++) {
+//             oldDiv[i].parentNode.removeChild(oldDiv[i]);
+//         }
+//         newArray = newArray.filter(value => value.age > 28)
+//         for (let user of newArray) {
+//             let userDiv = document.createElement('div')
+//             userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//             userBox.append(userDiv)
+//         }
+//     }
+//     if (!inp29.checked) {
+//         oldDiv = userBox.querySelectorAll('div');
+//         for (let i = 0, len = oldDiv.length; i < len; i++) {
+//             oldDiv[i].parentNode.removeChild(oldDiv[i]);
+//         }
+//
+//         for (let user of newArray) {
+//             let userDiv = document.createElement('div')
+//             userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//             userBox.append(userDiv)
+//         }
+//     }
+//
+//     if (inpKyiv.checked) {
+//         oldDiv = userBox.querySelectorAll('div');
+//         for (let i = 0, len = oldDiv.length; i < len; i++) {
+//             oldDiv[i].parentNode.removeChild(oldDiv[i]);
+//         }
+//         newArray = newArray.filter(value => value.address.city === 'Kyiv')
+//         for (let user of newArray) {
+//             let userDiv = document.createElement('div')
+//             userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//             userBox.append(userDiv)
+//         }
+//     }
+//     if (!inpKyiv.checked) {
+//         oldDiv = userBox.querySelectorAll('div');
+//         for (let i = 0, len = oldDiv.length; i < len; i++) {
+//             oldDiv[i].parentNode.removeChild(oldDiv[i]);
+//         }
+//
+//         for (let user of newArray) {
+//             let userDiv = document.createElement('div')
+//             userDiv.innerText = `${user.id},${user.name},${user.age},${user.status},${user.address.city},${user.address.street},${user.address.number}`
+//             userBox.append(userDiv)
+//         }
+//     }
+// }
 
 
 // - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
@@ -182,3 +182,56 @@ let functionForm = () => {
 //     }
 // })
 
+// *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
+// при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к следующему дочернему элементу (лежащему на одном уровне)
+// НО если у (какого-либо)дочеренего элемента есть дети, то нажатие "вперед" позволяет нам войти внутрь элемента и  выводит первого ребенка. и тд.
+//     Когда все дети заканчиваются, мы выходим из данного дочернего элемента и переходим к следующему, лежащему с ним на одном уровне
+let arrayOfItems = []
+let allItems = document.body.children
+console.log(allItems);
+let form = document.createElement('form')
+let butBack = document.createElement('button')
+let butNext = document.createElement('button')
+butBack.innerText = 'BACK'
+butNext.innerText = 'NEXT'
+document.body.append(form)
+form.append(butBack)
+form.append(butNext)
+let marker = 0
+let findItems = (array) => {
+    for (let element of array) {
+        arrayOfItems.push(element)
+        findItems(element.children)
+    }
+}
+findItems(allItems)
+let killDiv = document.createElement('div')
+killDiv.id = 'id'
+document.body.append(killDiv)
+butNext.addEventListener('click', e => {
+        e.preventDefault()
+        let killDiv = document.getElementById('id')
+        killDiv.parentNode.removeChild(killDiv)
+        let div = document.createElement('div')
+        div.id = 'id'
+        div.innerHTML = `tag: ${arrayOfItems[marker].tagName} /// classes: ${arrayOfItems[marker].classList} /// id: ${arrayOfItems[marker].id} /// text: ${arrayOfItems[marker].innerText} `
+        document.body.append(div)
+        if (marker < arrayOfItems.length - 1) {
+            marker += 1
+        } else marker = 0
+    }
+)
+butBack.addEventListener('click', e => {
+        e.preventDefault()
+        let killDiv = document.getElementById('id')
+        killDiv.parentNode.removeChild(killDiv)
+        if (marker > 0) {
+            --marker
+        } else marker = arrayOfItems.length - 1
+        let div = document.createElement('div')
+        div.id = 'id'
+        div.innerHTML = `tag: ${arrayOfItems[marker].tagName} , classes: ${arrayOfItems[marker].classList} , id: ${arrayOfItems[marker].id}`
+        document.body.append(div)
+
+    }
+)
